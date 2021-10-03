0b697f6f8924fc0811221a48b5e54a8b78d96a88
Vinh Nguyen <zinh@users.noreply.github.com>
2021-10-03 11:49:13 +0900

    index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author
 
 # Create your views here.
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instance_available = BookInstance.objects.filter(
+        status__exact='a').count()
+    num_authors = Author.objects.all().count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_authors': num_authors,
+        'num_instance_available': num_instance_available
+    }
+    return render(request, 'index.html', context)
